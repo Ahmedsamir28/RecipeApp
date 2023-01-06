8e7cb01969f10efdd2d64998fd884f3a2fa4e3fa
Ahmed Samir <107774249+Ahmedsamir28@users.noreply.github.com>
2023-01-06 21:01:34 +0200

    a

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 // www.themealdb.com/api/json/v1/1/filter.php?c=Seafood  by categories
 // www.themealdb.com/api/json/v1/1/lookup.php?i=52772    by id 
 
+const searchInput = document.getElementsByClassName("search-input")
+const searchButton = document.getElementsByClassName("search-butt")
